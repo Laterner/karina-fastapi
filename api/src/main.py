7e8532a899ef113a5f16351f93b5d6eb7628f5cc
Laterner <ivantrofimov2001@gmail.com>
2023-09-16 01:07:37 +0300

    cors

--- api/src/main.py
+++ api/src/main.py
@@ -9,14 +9,26 @@ from uuid import uuid4
 
 app = FastAPI(debug=True)
 
-origins = ["*"]
+# origins = ["*"]
+
+origins = [
+    "http://localhost",
+    "http://localhost:80",
+    "http://localhost:8888",
+    "http://localhost:3000",
+    
+    "http://185.221.162.85",
+    "http://185.221.162.85:80",
+    "http://185.221.162.85:8888",
+    "http://185.221.162.85:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type", "Access-Control-Request-Headers", "Access-Control-Allow-Methods", "Access-Control-Allow-Origin", "Authorization", "x-total-count"],
 )
 
 
